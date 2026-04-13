e969c3f5e6717a9f48e4e39d5b5369fe25944169
Henrique A. R. <henrique4r.it@gmail.com>
2026-04-12 22:36:55 -0300

    feat: adiciona a screen home

---
+++ src/screens/Home.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+
+import { styles } from './HomeStyles';
+
+export function Home() {
+
+    return (
+        <View style={styles.container}>
+
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+    )
+}
