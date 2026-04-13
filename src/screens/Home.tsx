a15fc11251289a5a3b09c5d0774720bfa47cd14c
Henrique A. R. <henrique4r.it@gmail.com>
2026-04-12 22:38:21 -0300

    feat: adiciona a imagem de perfil

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,15 @@
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Image, Text } from "react-native";
 
 import { styles } from './HomeStyles';
+import { Profile } from "../components/profile/Profile";
 
 export function Home() {
 
     return (
         <View style={styles.container}>
 
+            <Profile/>
             <Text>Open up App.tsx to start working on your app!</Text>
             <Text>Open up App.tsx to start working on your app!</Text>
             <Text>Open up App.tsx to start working on your app!</Text>
